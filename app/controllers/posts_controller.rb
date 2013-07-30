0fb1d53cfc19a88b14f724e5f3c35929d9de7db6
Abeeku <kwekubondzie@gmail.com>
2013-07-30 17:24:23 -0400

    hg

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,17 +9,20 @@ class PostsController < ApplicationController
   def create
 
   	@post = current_user.posts.build(params[:post])
-  	unless @post.content.to_s.strip.length == 0
-    @post.created_at = Time.now
-  	if @post.save
-  		redirect_to request.referrer
-  	else
-      flash[:notice] = 'Post could\'nt be saved'
+  	if @post.content.to_s.strip.length == 0
+      flash[:notice] = 'Write something in your post first!'
       redirect_to request.referrer
-  	end
+    else
+      @post.created_at = Time.now
+      if @post.save
+        redirect_to request.referrer
+      else
+        flash[:notice] = 'Post could\'nt be saved'
+        redirect_to request.referrer
+      end
+
   end
-  flash[:notice] = 'Write someting in your post first!'
-  redirect_to request.referrer
+
   end
 
 end
